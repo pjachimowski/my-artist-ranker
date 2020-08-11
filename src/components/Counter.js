4dd450d424cd7b663eb73f7d96a341c5dd1e568c
Patryk <p.jachimowski89@gmail.com>
2020-08-11 20:27:09 +0200

    BEFORE IMPLEMENTING OLDER CONTENT INTO NEW REDUX APP

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -7,14 +7,14 @@ const Counter = (props) => {
         className="counter-action decrement"
         onClick={() => props.updatePlayerScore(props.index, -1)}
       >
-        -
+        - -
       </button>
       <div className="counter-score"> {props.score} </div>
       <button
         className="counter-action increment"
         onClick={() => props.updatePlayerScore(props.index, 1)}
       >
-        +
+        + +
       </button>
     </div>
   );
